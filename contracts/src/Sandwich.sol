9bb1d96177f0c560336bc766c1b8496368995090
Bankk <106103625+BankkRoll@users.noreply.github.com>
2023-05-04 22:47:47 -0400

    Update Sandwich.sol

--- contracts/src/Sandwich.sol
+++ contracts/src/Sandwich.sol
@@ -8,16 +8,16 @@ import "./lib/SafeTransfer.sol";
 contract Sandwich {
     using SafeTransfer for IERC20;
 
-    // Authorized
+    // Authorized user
     address internal immutable user;
 
-    // transfer(address,uint256)
+    // transfer(address,uint256) function signature
     bytes4 internal constant ERC20_TRANSFER_ID = 0xa9059cbb;
 
-    // swap(uint256,uint256,address,bytes)
+    // swap(uint256,uint256,address,bytes) function signature
     bytes4 internal constant PAIR_SWAP_ID = 0x022c0d9f;
 
-    // Contructor sets the only user
+    // Constructor sets the only user
     receive() external payable {}
 
     constructor(address _owner) {
@@ -26,7 +26,7 @@ contract Sandwich {
 
     // *** Receive profits from contract *** //
     function recoverERC20(address token) public {
-        require(msg.sender == user, "shoo");
+        require(msg.sender == user, "Unauthorized");
         IERC20(token).safeTransfer(
             msg.sender,
             IERC20(token).balanceOf(address(this))
@@ -34,7 +34,7 @@ contract Sandwich {
     }
 
     /*
-        Fallback function where you do your frontslice and backslice
+        Fallback function for frontslice and backslice
 
         NO UNCLE BLOCK PROTECTION IN PLACE, USE AT YOUR OWN RISK
 
@@ -53,35 +53,37 @@ contract Sandwich {
         address memUser = user;
 
         assembly {
-            // You can only access teh fallback function if you're authorized
+            // Only the authorized user can access the fallback function
             if iszero(eq(caller(), memUser)) {
-                // Ohm (3, 3) makes your code more efficient
-                // WGMI
-                revert(3, 3)
+                revert(0, 0)
             }
 
-            // Extract out teh variables
-            // We don't have function signatures sweet saving EVEN MORE GAS
-
-            // bytes20
+            // Extract out the variables
             let token := shr(96, calldataload(0x00))
-            // bytes20
             let pair := shr(96, calldataload(0x14))
-            // uint128
             let amountIn := shr(128, calldataload(0x28))
-            // uint128
             let amountOut := shr(128, calldataload(0x38))
-            // uint8
             let tokenOutNo := shr(248, calldataload(0x48))
 
             // **** calls token.transfer(pair, amountIn) ****
-
-            // transfer function signature
             mstore(0x7c, ERC20_TRANSFER_ID)
-            // destination
             mstore(0x80, pair)
-            // amount
             mstore(0xa0, amountIn)
+
+            let s1 := call(sub(gas(), 5000), token, 0, 0x7c, 0x44, 0, 0)
+            if iszero(s1) {
+                revert(0, 0)
+            }
+
+            // ************
+            // calls pair.swap(
+            //     tokenOutNo == 0 ? amountOut : 0,
+            //     tokenOutNo == 1 ? amountOut : 0,
+            //     address(this),
+            //     new bytes(0)
+            // )
+            mstore(0x7c, PAIR_SWAP_ID)
+            // Use a single 'mstore' for both conditions
 
             let s1 := call(sub(gas(), 5000), token, 0, 0x7c, 0x44, 0, 0)
             if iszero(s1) {
@@ -90,35 +92,24 @@ contract Sandwich {
             }
 
             // ************
-            /* 
-                calls pair.swap(
-                    tokenOutNo == 0 ? amountOut : 0,
-                    tokenOutNo == 1 ? amountOut : 0,
-                    address(this),
-                    new bytes(0)
-                )
-            */
-
-            // swap function signature
+            // calls pair.swap(
+            //     tokenOutNo == 0 ? amountOut : 0,
+            //     tokenOutNo == 1 ? amountOut : 0,
+            //     address(this),
+            //     new bytes(0)
+            // )
             mstore(0x7c, PAIR_SWAP_ID)
-            // tokenOutNo == 0 ? ....
-            switch tokenOutNo
-            case 0 {
-                mstore(0x80, amountOut)
-                mstore(0xa0, 0)
-            }
-            case 1 {
-                mstore(0x80, 0)
-                mstore(0xa0, amountOut)
-            }
-            // address(this)
+            // Use a single 'mstore' for both conditions
+            mstore(
+                0x80,
+            (tokenOutNo == 0 ? amountOut : 0) | (tokenOutNo == 1 ? amountOut << 128 : 0)
+            )
             mstore(0xc0, address())
-            // empty bytes
             mstore(0xe0, 0x80)
 
             let s2 := call(sub(gas(), 5000), pair, 0, 0x7c, 0xa4, 0, 0)
             if iszero(s2) {
-                revert(3, 3)
+                revert(0, 0)
             }
         }
     }
